4f76e75e829e3ffd44240307848f9050a224415c
AlikhanBaisholanov <alikh3380944@gmail.com>
2020-04-13 00:17:44 +0600

    TESTING3

--- app/src/main/java/com/example/kino/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/kino/RecyclerViewAdapter.kt
@@ -11,7 +11,7 @@ import com.squareup.picasso.Picasso
 import java.util.*
 
 class RecyclerViewAdapter(
-    //TESTING7
+    //TESTING8
     var movies: List<Movie>? = null,
     val itemClickListener: RecyclerViewItemClick? = null
 
